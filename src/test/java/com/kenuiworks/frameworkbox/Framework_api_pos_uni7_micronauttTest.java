add4fe8acd8b492b405282ad7fa2c6798d703263
daniel baccin <daniel.baccin@gmail.com>
2021-04-04 12:16:40 -0300

    Implementações gerais do projeto

---
+++ src/test/java/com/kenuiworks/frameworkbox/Framework_api_pos_uni7_micronauttTest.java
@@ -0,0 +1,21 @@
+package com.kenuiworks.frameworkbox;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+class Framework_api_pos_uni7_micronauttTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
